d5153e97a25fd57eab0a49d47c35159ad30f1382
Amber <ajiles91@gmail.com>
2019-06-08 19:33:59 -0400

    3rd submission

--- script.js
+++ script.js
@@ -52,8 +52,8 @@ function renderResult(result) {
 
 
 function displayRecipeData(data) {
-  if (Response.ok === false) {
-    $('.error-catch').html(`<h2>no results found</h2>`)
+  if (response.ok === false) {
+    $('.error-catch').html(`<h2>No Results Found</h2>`)
   }
 
   else {
@@ -74,7 +74,7 @@ function displayRecipeData(data) {
 function makeList(array) {
   const list = document.createElement('ul'); 
  
-   for (let i=0; i<array.length; i++) {
+   for (let i = 0; i < array.length; i++) {
    const item = document.createElement('li'); 
    item.appendChild(document.createTextNode(array[i])); 
    list.appendChild(item); 
@@ -83,46 +83,12 @@ function makeList(array) {
 }
 
 
-// // Generate Nutrient Info List
-// function makeListNutrData(obj) {
-//   var satFatValues = Object.values(result.totalNutrients.FASAT);
-//   var calValues = Object.values(result.totalNutrients.ENERC_KCAL);
-//   var sugarValues = Object.values(result.totalNutrients.SUGAR);
-//   var fiberValues = Object.values(result.totalNutrients.FIBTG);
-//   var proteinValues = Object.values(result.totalNutrients.PROCNT);
-//   var carbsValues = Object.values(result.totalNutrients.CHOCDF);
 
-//   const paragraph = document.createElement("p");
-//   var br = document.createElement("br");
-//   var element = document.getElementsByClassName("nutrient-list");
-
-//   var satFatNode = document.createTextNode("`There's ${Math.round(satFatValues[1][1])}${satFatValues[2][1]} of ${satFatValues[0][1]}`");
-//   var calNode = document.createTextNode("`There's ${Math.round(calValues[1][1])}${calValues[2][1]} of ${calValues[0][1]}`");
-//   var sugarNode = document.createTextNode("`There's ${Math.round(sugarValues[1][1])}${sugarValues[2][1]} of ${sugarValues[0][1]}`");
-//   var fiberNode = document.createTextNode("`There's ${Math.round(fiberValues[1][1])}${fiberValues[2][1]} of ${fiberValues[0][1]}`");
-//   var proteinNode = document.createTextNode("`There's ${Math.round(proteinValues[1][1])}${proteinValues[2][1]} of ${proteinValues[0][1]}`");
-//   var carbsNode = document.createTextNode("`There's ${Math.round(carbsValues[1][1])}${carbsValues[2][1]} of ${carbsValues[0][1]}`");
-  
-//   para.appendChild(satFatNode);
-//   element.appendChild(br);
-//   para.appendChild(calNode);
-//   element.appendChild(br);
-//   para.appendChild(sugarNode);
-//   element.appendChild(br);
-//   para.appendChild(fiberNode);
-//   element.appendChild(br);
-//   para.appendChild(proteinNode);
-//   element.appendChild(br);
-//   para.appendChild(proteinNode);
-
-
-//   return list.outerHTML;
-// }
 const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
 const API_KEY = "AIzaSyDtt_G4qN2cPH4UIov4-H7VAyOCpSyX3NY";
 
 function getDataFromVideoApi(searchTerm, callback) {
-  const query= {
+  const query = {
     part: 'snippet', 
     key: API_KEY, 
     q: `${searchTerm} recipe`,
@@ -163,8 +129,6 @@ function watchSubmit() {
     const query = queryTarget.val(); 
     queryTarget.val(""); 
     getDataFromFoodApi(query, displayRecipeData);
-    
-
     $('.result-area').show(); 
   }); 
 }
